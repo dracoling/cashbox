f6338f196e1a22c445e20bc59a88735d0d0d5a70
Mike Skelly <dracoling@gmail.com>
2011-06-20 23:02:23 -0400

    Android Project

---
+++ src/net/smrsh/android/cashbox/CashboxActivity.java
@@ -0,0 +1,13 @@
+package net.smrsh.android.cashbox;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CashboxActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
